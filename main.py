4215b6c588404e8e4364baf726c1d7538567701d
1m70Deter <48923389+1m70Deter@users.noreply.github.com>
2022-05-15 18:36:20 +0200

    Mise à jour mode multi

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class Api:
     def getPosition(self, jid):
         # Renvoie les positions des éléments sous forme d'un objet (dictionnaire) mis en chaine de texte
         # Ce sont les positions de J2
-        url = "http://" + serverIP + "/asteroide-get.php?id=" + jid
+        url = "http://" + serverIP + "/recup.php?id=" + jid
         return requests.get(url).text
 
     def postPosition(self, j, jid):
@@ -49,12 +49,12 @@ class Api:
                 "y" : j["centre"]["y"]
             }
         }
-        url = "http://" + serverIP + "/asteroide-post.php"
+        url = "http://" + serverIP + "/postPos.php"
         return requests.post(url, donne).text
 
     def tirerBalle(self, jid, x, y, rt):
         # Cette fonction envoie la positions des tirs à la base de données
-        url = "http://" + serverIP + "/asteroide-get.php?tirer=" + str(jid) + "&x=" + str(x) + "&y=" + str(y) + "&a=" + str(rt)
+        url = "http://" + serverIP + "/tirer.php?id=" + str(jid) + "&x=" + str(x) + "&y=" + str(y) + "&a=" + str(rt)
         requests.get(url)
 
     def lancerMusiqueMenu(self):
@@ -63,25 +63,28 @@ class Api:
     
     def creerPartie(self):
         # Cette fonction crée une partie sur la base de données
-        return requests.get("http://" + serverIP + "/asteroide-get.php?createGame=1").text
+        return requests.get("http://" + serverIP + "/creer.php").text
 
     def rejoindrePartie(self, idj):
         # Cette fonction permet d'ajouter un joueur à la base de données grâce à une id de partie
-        return requests.get("http://" + serverIP + "/asteroide-get.php?joinGame=" + str(idj)).text
+        return requests.get("http://" + serverIP + "/rejoindre.php?id=" + str(idj)).text
 
     def quitterPartie(self, idj):
         # Cette fonction permet de retirer le joeur de la base de données
-        requests.get("http://" + serverIP + "/asteroide-get.php?quitGame=" + idj)
+        requests.get("http://" + serverIP + "/quitter.php?id=" + idj)
 
-    def ecrireConf(self, multij, jip, jid):
+    def ecrireConf(self, multij, jip, jid, sip):
         # Cette fonction permet d'écrire les valeurs multij, ip du joueur et id de la partie
         # Toutes ces valeurs sont enregistrées à l'intérieur du fichier conf.txt
         f = open("conf.txt", 'r')
         cnt = f.read().split("\n")
         f.close()
-        cnt[0] = cnt[0].split("=")[0] + "=" + str(jid)
-        cnt[1] = cnt[1].split("=")[0] + "=" + str(jip)
-        cnt[2] = cnt[2].split("=")[0] + "=" + str(multij)
+        if not sip == "":
+            cnt[3] = cnt[3].split("=")[0] + "=" + sip
+        else:
+            cnt[0] = cnt[0].split("=")[0] + "=" + str(jid)
+            cnt[1] = cnt[1].split("=")[0] + "=" + str(jip)
+            cnt[2] = cnt[2].split("=")[0] + "=" + str(multij)
         f = open("conf.txt", 'w')
         f.write("\n".join(cnt))
         f.close()
@@ -94,17 +97,24 @@ class Api:
         c = {
             "jid" : cnt[0].split("=")[1],
             "jip" : cnt[1].split("=")[1],
-            "multij" : cnt[2].split("=")[1]
+            "multij" : cnt[2].split("=")[1],
+            "sip" : cnt[3].split("=")[1]
         }
         return c
 
-    def writeScore(self, nom, pts, multij):
+    def writeScore(self, nom, pts, multij, idj):
         # Cette fonction écrit le score à l'intérieur d'une base de données
         # Si la partie est multijoueur, alors elle envoie une requête à ip/asteroide-get.php?quitGame=1
         # Sinon, elle sauvegarde le score à l'intérieur de la BDD
-        if multij == 1:
+        if int(multij) == 1:
             # Le serveur gère en cas de défaite ou de victoire de joueurs
-            requests.get("http://" + serverIP + "/asteroide-get.php?quitGame=" + idj)
+            rep = requests.get("http://" + serverIP + "/quitter.php?id=" + idj + "&nom=" + nom + "&score=" + str(pts)).text
+            rep = rep.split(",")
+            count = 0
+            for i in rep:
+                rep[count] = [count] + i.split("|")
+                count += 1
+            return rep
         else:
             # On se connecte à la base de données
             bdd = sqlite3.connect("sql/bdd.sqlite")
@@ -151,7 +161,11 @@ pygame.mixer.init()
 m_menu = pygame.mixer.Sound("html/asset/musiques/menu.mp3")
 m_main = pygame.mixer.Sound("html/asset/musiques/main.mp3")
 
-serverIP = input("ip> ")#"localhost"
+# Chargement de l'IP depuis les confs
+f = open("conf.txt", 'r')
+serverIP = f.read().split("\n")[3].split("=")[1]
+print("[INFO] IP du serveur Multijoueur : " + serverIP)
+f.close()
 
 # Pour plus de praticité dans l'exécution, on renomme la variable avant de la passer
 api=Api()
